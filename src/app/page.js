c888f288d1f3ef54fe485159567f056a5b437c93
samiursakib <rafisamiur@gmail.com>
2024-12-24 02:57:31 +0600

    gsap installation and setup ready and made an animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,24 @@
+"use client";
+
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 export default function Home() {
-  return <div>Home</div>;
+  const textRef = useRef(null);
+  useGSAP(() => {
+    gsap.to(textRef.current, {
+      duration: 1,
+      opacity: 1,
+      scale: 1.1,
+      ease: "bounce",
+    });
+  });
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-[20rem] font-extrabold opacity-0" ref={textRef}>
+        Home
+      </div>
+    </div>
+  );
 }
